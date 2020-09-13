0ac16f52c3031363163daffc0254a7fff64e389d
gatosyocora <gatosyocora36@gmail.com>
2020-09-13 20:51:05 +0900

    first commit

---
+++ ABC154/d/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC154
+{
+    class Program
+    {
+        // 1からnまでの期待値は中央値と同じになる
+        static float CalcMedianValue(int maxValue)
+        {
+            return maxValue % 2 == 1 ? 
+                        (float)(maxValue - (maxValue / 2)) :
+                        (float)(maxValue - (maxValue / 2) + 0.5);
+        }
+
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var (n, k) = (inputs[0], inputs[1]);
+            var maxValues = Console.ReadLine().Split(' ').Select(int.Parse);
+            var expectedValues = maxValues.Select(CalcMedianValue).ToArray();
+
+            double sum = Enumerable.Range(0, k).Sum(i => expectedValues[i]);
+            var maxSumValue = sum;
+
+            for (int i = 1; i <= n - k; i++) 
+            {
+                sum -= expectedValues[i - 1];
+                sum += expectedValues[i + k - 1];
+                if (maxSumValue < sum)
+                {
+                    maxSumValue = sum;
+                }
+            }
+
+            Console.WriteLine($"{maxSumValue:F12}");
+        }
+    }
+}
---
+++ ABC154/e/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace e
+{
+    class Program
+    {
+        // 未完成
+        static void Main(string[] args)
+        {
+            var (n, k) = (Console.ReadLine(), int.Parse(Console.ReadLine()));
+            int count = 0;
+            for (int i = 1; i <= n.Length; i++) 
+            {
+                int number = i;
+                int nonZeroCount = 0;
+                for (;;) 
+                {
+                    if (number % 10 != 0) nonZeroCount++;
+                    number = number / 10;
+
+                    if (number == 0 || nonZeroCount > k) break;
+                }
+                if (nonZeroCount == k) count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
